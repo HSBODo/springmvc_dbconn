60b8ad98783828b8ace2d583c3eb627b524b1705
HSBODo <vinsulill@gmaile.com>
2023-04-01 18:44:33 +0900

    1.회원 등록 및 목록 view 생성 2.model -> view 데이터 전송

--- src/main/java/pointman/springmvc_db/controller/MemberController.java
+++ src/main/java/pointman/springmvc_db/controller/MemberController.java
@@ -1,11 +1,19 @@
 package pointman.springmvc_db.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pointman.springmvc_db.domain.Member;
 import pointman.springmvc_db.service.MemberService;
 
+import java.util.List;
+
+@Slf4j
 @Controller //스프링 컨테이너에 bean으로 등록하여 관리하도록 설정 @Component 등록방식
 public class MemberController {
     private  MemberService memberService;
@@ -23,5 +31,24 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+    @PostMapping("/members/new")
+    public String create(Member member){
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+
+        return "members/memberList";
+    }
 
 }
